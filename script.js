2c7d75d64e4426e2191155732cb73d6121af7707
Nurkanat <nurkanat.doszhan@gmail.com>
2023-11-26 20:48:39 +0600

    lil changes

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let news = document.getElementsByClassName('news')[0];
 let card = `
   <div class="col-xl-3 col-lg-4 col-md-6 mb-5">
     <div class="card border-0">
-      <img class="card-img-top rounded-2" src="https://images.unsplash.com/photo-1699882236909-b66077950046" alt="Card image cap">
+      <div class="card-image">
+        <img class="card-img-top rounded-2" src="https://images.unsplash.com/photo-1699882236909-b66077950046" alt="Card image cap" />
+      </div>
       <div class="card-body px-0 py-2">
         <small class="d-block mb-2">09.12.2020</small>
         <h6 class="card-title mb-3 text-uppercase">Как сделать ремонт одному?</h6>
